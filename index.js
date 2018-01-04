016302dc96ff0c061c93ac09c4c3ced4a9d23dc7
chatbot <chatbot@users>
2018-01-04 02:47:20 -0800

    发

--- index.js
+++ index.js
@@ -26,3 +26,19 @@ app.get('/', (req, res) => {
 app.post('/quotes', (req, res) => {
   console.log(req.body)
 })
+
+/*
+//Original tutorial 
+
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+*/
