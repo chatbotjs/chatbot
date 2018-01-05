de1fec1d7f9460cc79be3845391bf6239b30ada9
chatbot <chatbot@users>
2018-01-05 11:56:04 -0800

    发

--- index.js
+++ index.js
@@ -84,25 +84,27 @@ wss.on('message', function incoming(msg) {
 	let csTime = new Date(tmp.getTime()+3600000*8)
 	let psTime = new Date(tmp.getTime()-3600000*8)
 	  
-	let cnTime = csTime.toLocaleDateString('zh-CN')+"*  | *"+csTime.toString().replace(/^.+?[0-9]\s([0-9]?[0-9]\:[0-9][0-9]\:[0-9][0-9])\sGMT.+?$/gi, "$1")
+	let cnTime = csTime.toLocaleDateString('zh-CN')+"  | *"+csTime.toString().replace(/^.+?[0-9]\s([0-9]?[0-9]\:[0-9][0-9]\:[0-9][0-9])\sGMT.+?$/gi, "$1")
 		
-	let enTime = psTime.toLocaleDateString('en-US')+"*  | *"+psTime.toString().replace(/^.+?[0-9]\s([0-9]?[0-9]\:[0-9][0-9]\:[0-9][0-9])\sGMT.+?$/gi, "$1")
+	let enTime = psTime.toLocaleDateString('en-US')+"  | *"+psTime.toString().replace(/^.+?[0-9]\s([0-9]?[0-9]\:[0-9][0-9]\:[0-9][0-9])\sGMT.+?$/gi, "$1")
 	
 	let randomColor = getRandomColor()
 	
 	let embedAdCn = new Discord.RichEmbed()
 		.setColor(randomColor)
 		.setAuthor(msg.name, null, 'https://kamadan.decltype.org/search/author%3A"'+encodeURIComponent(msg.name)+'"')
-		.setDescription(prettyPrintCn(msg.message))		
+		.addField(prettyPrintCn(msg.message),"")
+		//.setDescription(prettyPrintCn(msg.message))		
 		.setFooter(cnTime)
 		
 		
 	let embedAdEn = new Discord.RichEmbed()
 		.setColor(randomColor)
 		.setAuthor(msg.name, null, 'https://kamadan.decltype.org/search/author%3A"'+encodeURIComponent(msg.name)+'"')
-		.setDescription(prettyPrintEn(msg.message))		
+		.addField(prettyPrintEn(msg.message),"")
+		//.setDescription(prettyPrintEn(msg.message))		
 		.setFooter(enTime)				
-	
+	  
 	//"__**"+msg.name+"**__\n*"+cnTime+"*\n"+prettyPrintCn(msg.message)
 	//"__**"+msg.name+"**__\n*"+enTime+"*\n"+prettyPrintEn(msg.message)
 	
