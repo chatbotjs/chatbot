d30e4fa339532c20f06171de481edd149549d918
chatbot <chatbot@users>
2018-01-05 11:28:30 -0800

    发

--- index.js
+++ index.js
@@ -110,6 +110,35 @@ wss.on('message', function incoming(msg) {
 	
 	client.channels.get(adChnl_Cn).send({ embedAdCn })
 	client.channels.get(adChnl_En).send({ embedAdEn })
+	client.channels.get(adChnl_Cn).send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
+  }
+})
 		
 })
 //end websocket======================================================================
