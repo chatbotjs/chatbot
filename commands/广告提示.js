08514a6214bc71515970994ce2d7f5cb82484d7e
chatbot <chatbot@users>
2018-01-19 13:55:11 -0800

    发

--- commands/广告提示.js
+++ commands/广告提示.js
@@ -1,12 +1,13 @@
-exports.run = async (client, mongoConnect, adMode, msg, args) => {
-	//B/c the connection URL links directly to a database, no need to select it with: const db = client.db(dbName);
+exports.run = async (discordClient, mongoConnect, adMode, msg, args) => {
+	
 	console.log(adMode)
-	/*In traditional relational databases, associations are used to relate data from one table to another. MongoDB and similar NoSQL databases are document-oriented (there are no tables), so relationships are drawn between document-objects instead.  SEE: ObjectID function*/
+	/*In traditional relational databases, associations (probably means relations) are used to relate data from one table to another. MongoDB and similar NoSQL databases are document-oriented (there are no tables), so relationships are drawn between document-objects instead.  SEE: ObjectID function*/
   	
 	switch(adMode){
 		case "+":
 			if (args.length != 0){
-				mongoConnect.then(async function(database) {								
+				mongoConnect.then(async function(client) {
+					let database = client.db("discord")
 					let r = await database.collection("channelUsers").updateMany({_id:message.author.id}, { $addToSet: { notify: { $each: args } } }, {
 						upsert: true
 					})
@@ -17,7 +18,8 @@ exports.run = async (client, mongoConnect, adMode, msg, args) => {
 			break
 		case "-":
 			if (args.length != 0){
-				mongoConnect.then(async function(database) {		
+				mongoConnect.then(async function(client) {	
+					let database = client.db("discord")
 					let r = await database.collection("channelUsers").updateMany( {_id:message.author.id}, { $pullAll: { notify: args } } )
 				}).catch(console.error)
 			} else {
@@ -25,12 +27,14 @@ exports.run = async (client, mongoConnect, adMode, msg, args) => {
 			}
 			break
 		case "~":
-			mongoConnect.then(async function(database) {		
+			mongoConnect.then(async function(client) {	
+				let database = client.db("discord")
 				let r = await database.collection("channelUsers").updateMany( {_id:message.author.id}, { $set: { notify: [] } } )
 			}).catch(console.error)
 			break
 		default:		
-			mongoConnect.then(async function(database) {		
+			mongoConnect.then(async function(client) {	
+				let database = client.db("discord")
 				let r = await database.collection("channelUsers").find( {_id:message.author.id}).project( { "notify": 1, _id: 0 } ).toArray()
 				console.log(r)
 			}).catch(console.error)
