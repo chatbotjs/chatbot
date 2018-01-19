9c115e2d8cd8b425853e3fac5ed407c46dc16a6c
chatbot <chatbot@users>
2018-01-19 14:07:08 -0800

    发

--- commands/广告提示.js
+++ commands/广告提示.js
@@ -3,6 +3,8 @@ exports.run = async (discordClient, mongoConnect, adMode, msg, args) => {
 	console.log(adMode)
 	/*In traditional relational databases, associations (probably means relations) are used to relate data from one table to another. MongoDB and similar NoSQL databases are document-oriented (there are no tables), so relationships are drawn between document-objects instead.  SEE: ObjectID function*/
   	
+	//the db name is the one created on mlab, before creation of user
+	
 	switch(adMode){
 		case "+":
 			if (args.length != 0){
@@ -35,7 +37,7 @@ exports.run = async (discordClient, mongoConnect, adMode, msg, args) => {
 		default:		
 			mongoConnect.then(async function(client) {	
 				let database = client.db("chatbotjs")
-				let r = await database.collection("channelUsers").find( {_id:msg.author.id}).project( { "notify": 1, _id: 0 } ).toArray()
+				let r = await database.collection("channelUsers").find( {_id:msg.author.id}).project( { "notify": 1, _id: 0 } )//.toArray()
 				console.log(r)
 			}).catch(console.error)
 	}	
