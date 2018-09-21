5847297dab2d3e21657095b3ded9715d2c2c349e
huiji-baike <huiji-baike@email>
2018-09-20 17:15:54 -0700

    首

--- index.js
+++ index.js
@@ -70,8 +70,8 @@ client.on('ready', () => {
 client.on("guildMemberAdd", (member) => {  
 member.send("传送").catch(e=>{console.log(JSON.stringify(e))})
   //console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` )
-  consolde.log(member.id)
-  consolde.log(member.guild.id)
+  console.log(member.id)
+  console.log(member.guild.id)
   member.setVoiceChannel("487837630952767502").catch(e=>{console.log(JSON.stringify(e))})
   if (member.id == offlineAgentS){
 	  if (!member.roles.find("name", "管理员")){
