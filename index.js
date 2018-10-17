8df6774edfa5edbdcb12cd2145cb73566b559d50
huiji-baike <huiji-baike@email>
2018-10-17 01:18:52 -0700

    首

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ client.on("guildMemberAdd", (member) => {
 client.on("presenceUpdate", (oldMember, newMember) => {  
   if ((oldMember.presence.status == "offline") && (newMember.presence.status == "online")){
 	  if (newMember.guild.id == "487837630952767498"){
-			client.channels.get("487837630952767500").send(newMember.displayName+" 已上线 \:slight_smile:").catch(console.error)
+			client.channels.get("487837630952767500").send(newMember.displayName+" 已上线 \:slight_smile:").then(msg=>{msg.delete()}).catch(console.error)
 	}
   }
 });
